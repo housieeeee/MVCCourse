ce772a8f12b9118fd9258564f193f6ab6d5ca867
house <piyushkumarsometing@gmail.com>
2024-04-24 00:11:51 +0530

    Add project files.

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
